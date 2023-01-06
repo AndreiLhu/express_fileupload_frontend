5b9254df92dfea9a83f0c695f3c366cfe89fef58
AndreiLhu <leahu.andreilucian@gmail.com>
2023-01-06 12:10:08 +0100

    first commit - fileupload forntend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [fileUpload, setFileUpload] = useState(false);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setFileUpload(true);
+  };
+
+  const handleSubmission = () => {
+    const formData = new FormData();
+
+    formData.append('File', selectedFile);
+
+    fetch('http://localhost:5001/upload_files', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log('Success:', result);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>File Upload</h1>
+      <form encType="multipart/form-data" action="/upload_files" id="form">
+        <div className="input-group">
+          <input type="file" name="file" onChange={changeHandler} />
+          {fileUpload ? (
+            <div>
+              <p>Filename: {selectedFile.name}</p>
+              <p>Filetype: {selectedFile.type}</p>
+              <p>Size in bytes: {selectedFile.size}</p>
+              <p>
+                lastModifiedDate:
+                {selectedFile.lastModifiedDate.toLocaleDateString()}
+              </p>
+            </div>
+          ) : (
+            <p>Select a file to show details</p>
+          )}
+          <div>
+            <button onClick={handleSubmission}>Submit</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
